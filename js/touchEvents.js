f79a67021eb0fc2e286e529fc8e787672d2f8c59
Marqui <98498681+Marqui-13@users.noreply.github.com>
2022-10-18 20:46:13 -0700

    Update touchEvents.js

--- js/touchEvents.js
+++ js/touchEvents.js
@@ -1,16 +1,26 @@
+// NAVBAR
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
 window.addEventListener("touchstart", event => {
-                mouse.x = event.changedTouches[0].clientX;
-                mouse.y = event.changedTouches[0].clientY;
-            }, false);
+  mouse.x = event.changedTouches[0].clientX;
+  mouse.y = event.changedTouches[0].clientY;
+}, false);
             
-            window.addEventListener("touchmove", event => {
-                //event.preventDefault();
-                mouse.x = event.targetTouches[0].clientX;
-                mouse.y = event.targetTouches[0].clientY;
-            }, false);
+window.addEventListener("touchmove", event => {
+  //event.preventDefault();
+  mouse.x = event.targetTouches[0].clientX;
+  mouse.y = event.targetTouches[0].clientY;
+}, false);
             
-            window.addEventListener("touchend", event => {
-                //event.preventDefault();
-                mouse.x = 0;
-                mouse.y = 0;
-            }, false);
+window.addEventListener("touchend", event => {
+  //event.preventDefault();
+  mouse.x = 0;
+  mouse.y = 0;
+}, false);
