4d6dc17d3a0f99c78e4900dcb2fddd9089071eb2
Marqui <threeee@skiff.com>
2022-12-27 13:01:57 -0600

    Update indexMain.js

--- js/indexMain.js
+++ js/indexMain.js
@@ -89,7 +89,7 @@ image.addEventListener('load', function(){
                 }
             }
             this.angle += this.speed/20;
-            let movement = (3.5 - this.speed) + this.velocity;
+            let movement = (4.3 - this.speed) + this.velocity;
             this.y -= movement + Math.cos(this.angle) * 2;
             this.x += Math.cos(this.angle) * 2;
             if (this.y <= 0) {
