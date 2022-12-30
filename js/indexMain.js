5d80b8a9473fa32323450b41e8fd01f07f88234a
Marqui <threeee@skiff.com>
2022-12-30 12:47:52 -0800

    Update indexMain.js

--- js/indexMain.js
+++ js/indexMain.js
@@ -43,7 +43,7 @@ image.addEventListener('load', function(){
     canvas.height = 300;
 
     let particlesArray = [];
-    const numberOfParticles = 10000;
+    const numberOfParticles = 5000;
     const detail = 10;
 
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
