62ca4b2c1b92e831c2bb10a9133be9cadb28aa5e
Marqui <threeee@skiff.com>
2023-01-06 08:30:45 -0800

    Update indexMain.js

--- js/indexMain.js
+++ js/indexMain.js
@@ -44,7 +44,7 @@ image.addEventListener('load', function(){
 
     let particlesArray = [];
     const numberOfParticles = 10000;
-    const detail = 0.9;
+    const detail = 1;
 
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -124,10 +124,10 @@ image.addEventListener('load', function(){
     init();
 
     function animate () {
-        ctx.globalAlpha = 0.05;
+        ctx.globalAlpha = 0.9;
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.globalAlpha = 0.3;
+        ctx.globalAlpha = 0.9;
         for (let i = 0; i < particlesArray.length; i++) {
             particlesArray[i].update();
             ctx.globalAlpha = particlesArray[i].speed * 0.9;
